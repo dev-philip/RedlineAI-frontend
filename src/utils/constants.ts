7de00cf417a6f4d60513bf2b708f49572eaaf102
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2025-09-08 02:45:37 -0400

    Stop tracking utils/constant.ts and update .gitignore

--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const VITE_API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-export const VITE_GOOGLE_CLIENT_ID =
-  "548138763462-c7caq2o656gpmp57024bpvbnei77rctp.apps.googleusercontent.com";
